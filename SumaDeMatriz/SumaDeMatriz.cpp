279b2d66211a720933f0f57fbc900b4670091057
Yamil Yaluk <160165541+Siegfried177@users.noreply.github.com>
2025-02-27 20:43:17 -0300

    Works for 15x15 (in theory)

--- SumaDeMatriz/SumaDeMatriz.cpp
+++ SumaDeMatriz/SumaDeMatriz.cpp
@@ -1,26 +1,60 @@
 #include <iostream>
 using std::cout;
 
-const int M = 5; // M es la cantidad de filas
-const int N = 5; // N es la cantidad de columnas
-//int matriz[N][N] = { {1,2}, {3,4} };
-//int matriz[N][N] = { {1,2,3}, {4,5,6}, {7,8,9} };
-int matriz[M][N] = {
+const int M = 15; // M es la cantidad de filas
+const int N = 15; // N es la cantidad de columnas
+int find_max();
+
+/*int matriz[M][N] = {
 		{  7,  53, 183, 439, 863 },
 		{ 497, 383, 563,  79, 973 },
 		{ 287,  63, 343, 169, 583 },
 		{ 627, 343, 773, 959, 943 },
 		{ 767, 473, 103, 699, 303 }
+};*/
+
+int matriz[N][N] = {
+		{  7,  53, 183, 439, 863, 497, 383, 563,  79, 973, 287,  63, 343, 169, 583 },
+		{ 627, 343, 773, 959, 943, 767, 473, 103, 699, 303, 957, 703, 583, 639, 913 },
+		{ 447, 283, 463,  29,  23, 487, 463, 993, 119, 883, 327, 493, 423, 159, 743 },
+		{ 217, 623,   3, 399, 853, 407, 103, 983,  89, 463, 290, 516, 212, 462, 350 },
+		{ 960, 376, 682, 962, 300, 780, 486, 502, 912, 800, 250, 346, 172, 812, 350 },
+		{ 870, 456, 192, 162, 593, 473, 915,  45, 989, 873, 823, 965, 425, 329, 803 },
+		{ 973, 965, 905, 919, 133, 673, 665, 235, 509, 613, 673, 815, 165, 992, 326 },
+		{ 322, 148, 972, 962, 286, 255, 941, 541, 265, 323, 925, 281, 601,  95, 973 },
+		{ 445, 721,  11, 525, 473,  65, 511, 164, 138, 672,  18, 428, 154, 448, 848 },
+		{ 414, 456, 310, 312, 798, 104, 566, 520, 302, 248, 694, 976, 430, 392, 198 },
+		{ 184, 829, 373, 181, 631, 101, 969, 613, 840, 740, 778, 458, 284, 760, 390 },
+		{ 821, 461, 843, 513,  17, 901, 711, 993, 293, 157, 274,  94, 192, 156, 574 },
+		{  34, 124,   4, 878, 450, 476, 712, 914, 838, 669, 875, 299, 823, 329, 699 },
+		{ 815, 559, 813, 459, 522, 788, 168, 586, 966, 232, 308, 833, 251, 631, 107 },
+		{ 813, 883, 451, 509, 615,  77, 281, 613, 459, 205, 380, 274, 302,  35, 805 }
 };
+
 const int O = M > N ? N : M; // O es el valor mas pequeño entre M y N
-int nums_max[O], sum_max = 0, nums[N], forbidden[N] = { 0, 0, 0 };
+int nums_max[O], sum_max = 0, nums[O], forbidden[O], max = find_max();
+
+void make0() {
+	for (int i = 0; i < O; i++) {
+		forbidden[i] = 0;
+	}
+}
+
+int find_max() {
+	for (int i = 0; i < O; i++)
+		for (int j = 0; j < O; j++) 
+			if (max < matriz[i][j]) max = matriz[i][j];
+	return max;
+}
 
 void calculate(int n, int sum) {
 	//cout << "Enter\n";
 	if (n == O) return; // Caso base
 
 	for (int i = 0; i < N; i++) {
+		//cout << "\n";
 		if (forbidden[i]) continue; // Si es una columna prohibida se salta
+		if (sum + (max * (N - n)) < sum_max) continue;
 		sum += matriz[n][i];
 		//cout << "Suma: " << sum << "\n";
 		//cout << "Actual: " << matriz[n][i] << "\n";
@@ -46,9 +80,11 @@ void calculate(int n, int sum) {
 
 int main()
 {
+	make0();
+	cout << max << "\n";
 	calculate(0, 0);
-	cout << "Suma Maxima: " << sum_max;
+	cout << "Suma Maxima: " << sum_max << "\n";
 	for (int j = 0; j < N; j++) {
-		cout << " " << nums_max[j];
+		cout << nums_max[j] << " ";
 	}
 }
